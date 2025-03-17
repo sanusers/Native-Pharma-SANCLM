f2c6af2aa0df83862fab5fdedd2925daeb6d4089
Dinesh Prakash <knightdinesh476@gmail.com>
2025-03-17 15:42:34 +0530

    delete call activity setup added

--- app/src/main/java/saneforce/sanzen/activity/homeScreen/adapters/Call_adapter.java
+++ app/src/main/java/saneforce/sanzen/activity/homeScreen/adapters/Call_adapter.java
@@ -216,6 +216,7 @@ public class Call_adapter extends RecyclerView.Adapter<Call_adapter.listDataView
             jsonObject.put("CusType", type);
             jsonObject.put("sample_validation", SharedPref.getSampleValidation(context));
             jsonObject.put("input_validation",  SharedPref.getInputValidation(context));
+            jsonObject.put("activitynd", SharedPref.getActivityNd(context));
             Log.v("delCall", jsonObject.toString());
         } catch (Exception e) {
             Log.v("delCall", e.toString());
