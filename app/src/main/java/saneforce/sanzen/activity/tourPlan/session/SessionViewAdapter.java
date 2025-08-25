ecf01cc379b0981a75c56a9e622a0a7c7d2d560b
Dinesh Prakash <knightdinesh476@gmail.com>
2025-08-25 17:17:38 +0530

    Multi HQ selection TP based DCR updates

--- app/src/main/java/saneforce/sanzen/activity/tourPlan/session/SessionViewAdapter.java
+++ app/src/main/java/saneforce/sanzen/activity/tourPlan/session/SessionViewAdapter.java
@@ -144,13 +144,31 @@ public class SessionViewAdapter extends RecyclerView.Adapter<SessionViewAdapter.
         }
 
         //Joint Work
-        if (holder.data.getJC().size() > 0) {
+        if (!holder.data.getJC().isEmpty() && !isMGR) {
             StringBuilder jcName = new StringBuilder();
             for (int i = 0; i < holder.jcModelArray.size(); i++) {
                 if (jcName.length() == 0) {
                     jcName = new StringBuilder(holder.jcModelArray.get(i).getName());
                 } else {
                     jcName.append(", ").append(holder.jcModelArray.get(i).getName());
+                }
+            }
+            if (jcName.length() > 0) {
+                holder.jcTV.setText(jcName);
+            }
+        } else if(!holder.data.getJCs().isEmpty() && isMGR) {
+            holder.jcLayout.setVisibility(View.VISIBLE);
+            StringBuilder jcName = new StringBuilder();
+            for (int i = 0; i < holder.jcsModelArray.size(); i++) {
+                MultiHQHeaderModelClass multiHQHeaderModelClass = holder.jcsModelArray.get(i);
+                ArrayList<MultiHQItemModelClass> list = multiHQHeaderModelClass.getItemsList();
+                for (int j = 0; j < list.size(); j++) {
+                    MultiHQItemModelClass multiHQItemModelClass = list.get(j);
+                    if (jcName.length() == 0) {
+                        jcName = new StringBuilder(multiHQItemModelClass.getName());
+                    } else {
+                        jcName.append(", ").append(multiHQItemModelClass.getName());
+                    }
                 }
             }
             if (jcName.length() > 0) {
