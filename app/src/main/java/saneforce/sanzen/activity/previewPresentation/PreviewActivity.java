f51eba6a40b38184e37832bbd13c5c704b982aa7
Dinesh Prakash <knigthdinesh476@gmail.com>
2025-08-29 23:05:18 +0530

    Custom Presentation Creation

--- app/src/main/java/saneforce/sanzen/activity/previewPresentation/PreviewActivity.java
+++ app/src/main/java/saneforce/sanzen/activity/previewPresentation/PreviewActivity.java
@@ -42,13 +42,11 @@ import saneforce.sanzen.activity.call.dcrCallSelection.DcrCallTabLayoutActivity;
 import saneforce.sanzen.activity.call.pojo.detailing.CallDetailingList;
 import saneforce.sanzen.activity.call.pojo.detailing.StoreImageTypeUrl;
 import saneforce.sanzen.activity.homeScreen.HomeDashBoard;
-import saneforce.sanzen.activity.presentation.customerSelection.CustomerSelectionActivity;
 import saneforce.sanzen.activity.presentation.customerSelection.model.CustomerDataModel;
-import saneforce.sanzen.activity.presentation.presentation.PresentationActivity;
-import saneforce.sanzen.activity.presentation.presentation.ShowSideScreenListener;
 import saneforce.sanzen.activity.presentation.presentation.adapter.SideScreenAdapter;
 import saneforce.sanzen.activity.previewPresentation.fragment.BrandMatrix;
 import saneforce.sanzen.activity.previewPresentation.fragment.CustomPresentationFragment;
+import saneforce.sanzen.activity.previewPresentation.fragment.CustomizedPresentationFragment;
 import saneforce.sanzen.activity.previewPresentation.fragment.CustomPreviewFragment;
 import saneforce.sanzen.activity.previewPresentation.fragment.MyPresentation;
 import saneforce.sanzen.activity.previewPresentation.fragment.HomeBrands;
@@ -194,8 +192,10 @@ public class PreviewActivity extends AppCompatActivity {
                     viewPagerAdapter.add(new MyPresentation(), getResources().getString(R.string.my_presentation));
                     headingData.add("F");
                 }
-                viewPagerAdapter.add(new CustomPresentationFragment(), getResources().getString(R.string.custom_presentation));
+                viewPagerAdapter.add(new CustomizedPresentationFragment(), getResources().getString(R.string.customized_presentation));
                 headingData.add("G");
+                viewPagerAdapter.add(new CustomPresentationFragment(), getResources().getString(R.string.custom_presentation));
+                headingData.add("H");
             } else {
                 viewPagerAdapter.add(new WelcomePresentation(), getResources().getString(R.string.welcome));
                 headingData.add("A");
@@ -211,8 +211,10 @@ public class PreviewActivity extends AppCompatActivity {
                     viewPagerAdapter.add(new MyPresentation(), getResources().getString(R.string.my_presentation));
                     headingData.add("F");
                 }
-                viewPagerAdapter.add(new CustomPresentationFragment(), getResources().getString(R.string.custom_presentation));
+                viewPagerAdapter.add(new CustomizedPresentationFragment(), getResources().getString(R.string.custom_presentation));
                 headingData.add("G");
+                viewPagerAdapter.add(new CustomPresentationFragment(), getResources().getString(R.string.custom_presentation));
+                headingData.add("H");
             }
         } else {
             viewPagerAdapter.add(new HomeBrands(), getResources().getString(R.string.all_brands));
--- app/src/main/java/saneforce/sanzen/activity/previewPresentation/fragment/CustomPresentationFragment.java
+++ app/src/main/java/saneforce/sanzen/activity/previewPresentation/fragment/CustomPresentationFragment.java
@@ -1,40 +1,30 @@
 package saneforce.sanzen.activity.previewPresentation.fragment;
 
-import static saneforce.sanzen.activity.previewPresentation.PreviewActivity.CusType;
-import static saneforce.sanzen.activity.previewPresentation.PreviewActivity.cus_code;
-
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-
 import saneforce.sanzen.R;
-import saneforce.sanzen.activity.presentation.createPresentation.BrandModelClass;
-import saneforce.sanzen.activity.presentation.presentation.adapter.PresentationAdapter;
 import saneforce.sanzen.commonClasses.CommonUtilsMethods;
+import saneforce.sanzen.databinding.FragmentCustomPresentationBinding;
+import saneforce.sanzen.databinding.FragmentCustomerPresentationBinding;
 import saneforce.sanzen.databinding.FragmentHomePreviewBinding;
 import saneforce.sanzen.roomdatabase.PresentationTableDetails.PresentationDataDao;
 import saneforce.sanzen.roomdatabase.RoomDB;
 
-
 public class CustomPresentationFragment extends Fragment {
-    private FragmentHomePreviewBinding binding;
-    PresentationAdapter presentationAdapter;
-    public static ArrayList<BrandModelClass.Presentation> SlideCustomList = new ArrayList<>();
+    private FragmentCustomPresentationBinding binding;
     private CommonUtilsMethods commonUtilsMethods;
     private RoomDB roomDB;
     private PresentationDataDao presentationDataDao;
-    
+
+    public CustomPresentationFragment() {
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,51 +32,12 @@ public class CustomPresentationFragment extends Fragment {
         presentationDataDao = roomDB.presentationDataDao();
         commonUtilsMethods = new CommonUtilsMethods(requireContext());
         commonUtilsMethods.setUpLanguage(requireContext());
-        SlideCustomList = presentationDataDao.getPresentations(CusType, cus_code);
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentHomePreviewBinding.inflate(inflater);
-
-        binding.tvAz.setOnClickListener(view -> {
-            binding.tvAz.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bg_purple_left_radius));
-            binding.tvAz.setTextColor(ContextCompat.getColor(requireContext(), R.color.white));
-            binding.tvZa.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bg_white_right));
-            binding.tvZa.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_purple));
-            presentationAdapter = new PresentationAdapter(requireContext(), SlideCustomList, "customized");
-            binding.rvBrandList.setLayoutManager(new GridLayoutManager(requireContext(), 4, GridLayoutManager.VERTICAL, false));
-            binding.rvBrandList.setAdapter(presentationAdapter);
-            Collections.sort(SlideCustomList, Comparator.comparing(BrandModelClass.Presentation::getPresentationName));
-        });
-
-        binding.tvZa.setOnClickListener(view -> {
-            binding.tvZa.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bg_purple_right_radius));
-            binding.tvZa.setTextColor(ContextCompat.getColor(requireContext(), R.color.white));
-            binding.tvAz.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bg_white_left));
-            binding.tvAz.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_purple));
-            presentationAdapter = new PresentationAdapter(requireContext(), SlideCustomList, "customized");
-            binding.rvBrandList.setLayoutManager(new GridLayoutManager(requireContext(), 4, GridLayoutManager.VERTICAL, false));
-            binding.rvBrandList.setAdapter(presentationAdapter);
-            Collections.sort(SlideCustomList, Comparator.comparing(BrandModelClass.Presentation::getPresentationName).reversed());
-        });
-
-        populateAdapter();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCustomPresentationBinding.inflate(inflater);
 
         return binding.getRoot();
     }
-    
-    public void populateAdapter() {
-        if (!SlideCustomList.isEmpty()) {
-            binding.constraintNoData.setVisibility(View.GONE);
-            binding.rvBrandList.setVisibility(View.VISIBLE);
-            presentationAdapter = new PresentationAdapter(requireContext(), SlideCustomList, "customized");
-            binding.rvBrandList.setLayoutManager(new GridLayoutManager(requireContext(), 4, GridLayoutManager.VERTICAL, false));
-            binding.rvBrandList.setAdapter(presentationAdapter);
-        } else {
-            binding.constraintNoData.setVisibility(View.VISIBLE);
-            binding.rvBrandList.setVisibility(View.GONE);
-        }
-    }
-    
 }
