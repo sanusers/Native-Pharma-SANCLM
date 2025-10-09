191f740f5fae84760111de50978a3887e18f6690
Dinesh Prakash <knightdinesh476@gmail.com>
2025-10-09 17:32:57 +0530

    Workplan Cluster Edit API save update

---
+++ app/src/main/java/saneforce/sanzen/commonClasses/SafeClickListener.java
@@ -0,0 +1,23 @@
+package saneforce.sanzen.commonClasses;
+
+import android.util.Log;
+import android.view.View;
+
+public abstract class SafeClickListener implements View.OnClickListener {
+    private static final long MIN_CLICK_INTERVAL = 1000; // in ms
+    private long lastClickTime;
+
+    public abstract void onSafeClick(View view);
+
+    @Override
+    public final void onClick(View view) {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastClickTime < MIN_CLICK_INTERVAL) {
+            return;
+        }
+        Log.i("SAFE click", "onClick: " + currentTime);
+        lastClickTime = currentTime;
+        onSafeClick(view);
+    }
+}
+
