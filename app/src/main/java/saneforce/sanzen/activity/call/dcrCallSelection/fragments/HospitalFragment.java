095a6ad3787df6e2b12ac2ebfa6da6613911f225
Dinesh Prakash <knightdinesh476@gmail.com>
2025-10-28 14:31:47 +0530

    HQ selector changes

--- app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/fragments/HospitalFragment.java
+++ app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/fragments/HospitalFragment.java
@@ -44,7 +44,6 @@ import saneforce.sanzen.activity.homeScreen.HomeDashBoard;
 import saneforce.sanzen.commonClasses.SafeClickListener;
 import saneforce.sanzen.activity.call.dcrCallSelection.DcrCallTabLayoutActivity;
 import saneforce.sanzen.activity.call.dcrCallSelection.adapter.AdapterDCRCallSelection;
-import saneforce.sanzen.activity.homeScreen.fragment.worktype.WorkPlanFragment;
 import saneforce.sanzen.activity.map.custSelection.CustList;
 import saneforce.sanzen.commonClasses.CommonUtilsMethods;
 import saneforce.sanzen.commonClasses.Constants;
@@ -183,13 +182,13 @@ public class HospitalFragment extends Fragment {
                     listView.setOnItemClickListener((adapterView, view1, position, l) -> {
                         String selectedHq = listView.getItemAtPosition(position).toString();
                         tv_hqName.setText(selectedHq);
+                        String hqID = "", hqName = "";
                         for (int i = 0; i<jsonArray.length(); i++) {
                             try {
                                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                                 if(jsonObject.getString("name").equalsIgnoreCase(selectedHq)) {
-                                    DcrCallTabLayoutActivity.TodayPlanSfCode = jsonObject.getString("id");
-                                    DcrCallTabLayoutActivity.TodayPlanSfName = jsonObject.getString("name");
-                                    SharedPref.saveHq(requireContext(), DcrCallTabLayoutActivity.TodayPlanSfName, DcrCallTabLayoutActivity.TodayPlanSfCode);
+                                    hqID = jsonObject.getString("id");
+                                    hqName = jsonObject.getString("name");
                                     break;
                                 }
                             } catch (JSONException e) {
@@ -198,7 +197,7 @@ public class HospitalFragment extends Fragment {
                         }
 //                        DcrCallTabLayoutActivity.prepareClusterList(requireActivity());
 //                        SetupAdapter();
-                        hqChangeListener.onHQChange();
+                        hqChangeListener.onHQChange(hqID, hqName);
                         dialog.dismiss();
                     });
 
