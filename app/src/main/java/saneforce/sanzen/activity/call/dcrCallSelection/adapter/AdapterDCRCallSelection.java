d005a77db7a9328b378981c1140a4e5f5d57c7bc
Rithik Varshan <rithik02.2002varshan@gmail.com>
2026-01-09 18:43:21 +0530

    San Zen Issues Fix.

--- app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/adapter/AdapterDCRCallSelection.java
+++ app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/adapter/AdapterDCRCallSelection.java
@@ -177,7 +177,7 @@ public class AdapterDCRCallSelection extends RecyclerView.Adapter<AdapterDCRCall
 
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        if (jsonObject.getString("Dcr_dt").equalsIgnoreCase(HomeDashBoard.selectedDate.toString()) && jsonObject.getString("CustCode").equalsIgnoreCase(cusListArrayList.get(position).getCode())) {
+                        if (jsonObject.getString("Dcr_dt").equalsIgnoreCase(HomeDashBoard.selectedDate.toString()) && jsonObject.getString("CustCode").equalsIgnoreCase(cusListArrayList.get(position).getCode()) && jsonObject.getString("CustType").equalsIgnoreCase(cusListArrayList.get(position).getType())) {
                             isVisitedToday = true;
                             break;
                         }
