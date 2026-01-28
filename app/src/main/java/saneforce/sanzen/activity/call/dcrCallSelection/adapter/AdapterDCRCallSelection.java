3ed90bc2f71ff671c7ea695ccf203c3dd8a6968c
Rithik Varshan <rithik02.2002varshan@gmail.com>
2026-01-28 19:18:09 +0530

    Set 6 Fencing Info in DCR.

--- app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/adapter/AdapterDCRCallSelection.java
+++ app/src/main/java/saneforce/sanzen/activity/call/dcrCallSelection/adapter/AdapterDCRCallSelection.java
@@ -214,7 +214,7 @@ public class AdapterDCRCallSelection extends RecyclerView.Adapter<AdapterDCRCall
             }
         });
 
-       /* if (isFencing) {
+        if (isFencing) {
             holder.info.setVisibility(View.VISIBLE);
             holder.info.setOnClickListener(view -> {
                 String address = cusListArrayList.get(position).getAddress();
@@ -225,7 +225,7 @@ public class AdapterDCRCallSelection extends RecyclerView.Adapter<AdapterDCRCall
             });
         } else {
             holder.info.setVisibility(View.GONE);
-        }*/
+        }
     }
 
     private void showTimelinePopUp(View view, String latitude, String longitude, String address) {
