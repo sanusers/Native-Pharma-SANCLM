9cace2be5730aba3f6d2585d913c54888e00535b
Sakthi Priya <sridharsaneforce14@gmail.com>
2026-03-20 12:26:25 +0530

    language updates

--- app/src/main/java/saneforce/sanzen/activity/map/MapsActivity.java
+++ app/src/main/java/saneforce/sanzen/activity/map/MapsActivity.java
@@ -983,7 +983,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         jsonObject.put("image_name", imageName);
                         jsonObject.put("sfcode", SfCode);
                         jsonObject.put("addr", tv_address.getText().toString());
-                        jsonObject.put("addr", "6PJR+2WH - Al Mu'tarid -Oud Al Hassah - Abu Dhabi-United Arab States");
+                       // jsonObject.put("addr", "6PJR+2WH - Al Mu'tarid -Oud Al Hassah - Abu Dhabi-United Arab States");
                         if (SfType.equalsIgnoreCase("1")) {
                             jsonObject.put("tagged_cust_HQ", SfCode);
                         } else {
